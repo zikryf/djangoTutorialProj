25c90c66e69796d26acb31236e44989b600e482d
z1kwy <m07zikry@gmail.com>
2023-03-30 21:35:07 +0800

    added age field in models

--- django_tutorial/members/models.py
+++ django_tutorial/members/models.py
@@ -4,3 +4,4 @@ from django.db import models
 class Member(models.Model):
     firstname = models.CharField(max_length=255)
     lastname = models.CharField(max_length=255)
+    age = models.IntegerField()
